e5bed3baa5170a825e4fe121b2bacc6f2fd6a7b7
luojiong <luojion11@qq.com>
2018-01-31 20:36:45 +0800

    整改代码

--- book.java
+++ book.java
@@ -1,49 +1,54 @@
-public class book{
-public static void main(Stirng[] args){
-	String age="sfreogjrioegjioer";
-	System.out.println("sfheuw");
-	age=getInfr(age);
-}
-}
-public static String getInfr(String age){
-	return age.substring(0,age.length-2);
-}
+import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 
-public Integer setCount(Integer i){
-	return i;
-}
-public String getName(String name){
-return this.name=name;
+public class book {
+    private String name;
 
-public boolean getOffset()
-{
-int s=20;
-boolean flag=false;
+    public static void main(String[] args) {
+        String age = "sfreogjrioegjioer";
+        System.out.println("sfheuw");
+        age = getInfr(age);
+    }
 
+
+    public static String getInfr(String age) {
+        return age.substring(0, age.length() - 2);
+    }
+
+    public Integer setCount(Integer i) {
+        return i;
+    }
+
+    public String getName(String name) {
+        return this.name = name;
+    }
+
+    public boolean getOffset() {
+        return false;
+    }
 }
-public Object login(String loginacct , String userpswd,HttpSession session) {
-		start();
-		
-		try {
-			Member member = memberService.queryMemberByLogin(loginacct);
-			
-			if(member!=null) {
-				if(userpswd.equals(member.getUserpswd())) {
-					
-					session.setAttribute(Const.LOGIN_MEMBER, member);
-					
-					success(true);
-				}else {
-					success(false);
-				}
-				
-			}else {
-				success(false);
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return end();
-	}
+      /*  public Object login (String loginacct, String userpswd, HttpSession session){
+            // start();
+
+            try {
+                Member member = memberService.queryMemberByLogin(loginacct);
+
+                if (member != null) {
+                    if (userpswd.equals(member.getUserpswd())) {
+
+                        session.setAttribute(Const.LOGIN_MEMBER, member);
+
+                        //success(true);
+                    } else {
+                        //success(false);
+                    }
+
+                } else {
+                    //success(false);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return "";
+        }*/
